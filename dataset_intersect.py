aebc2ac71e88832c70e544b5c7cfafd23922b7c6
Richard <93225417+RiSchmi@users.noreply.github.com>
2024-10-19 20:43:00 +0300

    Add files via upload

--- dataset_intersect.py
+++ dataset_intersect.py
@@ -2,6 +2,9 @@
 import pandas as pd 
 
 lai_hourly = pd.read_csv('datasets/df_lai_factor.csv')
+main_traffic = pd.read_csv('datasets/df_traffic_hours.csv')
+
+
 sites_coord = pd.read_csv('datasets/df_spatial_only.csv')
 sites_coord['id'] = sites_coord['id'].apply(lambda x : x.lower().replace(' ', '')[:5])
 
@@ -12,6 +15,8 @@ df_2023_imputed = pd.merge(NO2_hourly, sites_coord, on = 'id', how= 'outer') # b
 # load and merge meteorological data (w/ imputed missing values)
 meteorological_hourly = pd.read_csv('datasets/df_meteorological_impute.csv')
 df_2023_imputed = pd.merge(df_2023_imputed, meteorological_hourly, on = 'time_step', how= 'inner') # bind
+df_2023_imputed = pd.merge(df_2023_imputed, main_traffic, on = 'time_step', how= 'inner') # bind
+
 
 # merge leaf area data
 df_2023_imputed = pd.merge(df_2023_imputed, lai_hourly, on = 'time_step', how= 'inner') # bind
@@ -30,6 +35,8 @@ df_2023_na = pd.merge(NO2_hourly, sites_coord, on = 'id', how= 'outer') # bind
 # load and merge meteorological data (w/ imputed missing values)
 meteorological_hourly = pd.read_csv('datasets/df_meteorological_na.csv')
 df_2023_na = pd.merge(df_2023_na, meteorological_hourly, on = 'time_step', how= 'inner') # bind
+df_2023_na = pd.merge(df_2023_na, main_traffic, on = 'time_step', how= 'inner')
+
 
 # merge lai
 df_2023_na = pd.merge(df_2023_na, lai_hourly, on = 'time_step', how= 'inner') # bind
